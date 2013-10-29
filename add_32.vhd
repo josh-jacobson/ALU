5069492846a07d0b2c934c960ad2584cc6ab4e40
Josh Jacobson <joshjacobson14@gmail.com>
2013-10-28 21:03:35 -0500

    Fixing subtract

--- add_32.vhd
+++ add_32.vhd
@@ -4,6 +4,7 @@ use ieee.std_logic_1164.all;
 entity add_32 is
   port (
     x, y   : in  std_logic_vector(31 downto 0);
+    carry_in : in std_logic;
     z   : out std_logic_vector(31 downto 0);
     overflow : out std_logic
   );
@@ -23,7 +24,7 @@ end component add;
 signal carry_out : std_logic_vector(31 downto 0);
 
 begin
-    add_0_map: add port map (a => x(0), b => y(0), c_in => '0', z => z(0), c_out => carry_out(0));
+    add_0_map: add port map (a => x(0), b => y(0), c_in => carry_in, z => z(0), c_out => carry_out(0));
     
     map_bits: for i in 1 to 31 generate
       sum1to31: add port map ( x(i), y(i), carry_out(i-1), z(i), carry_out(i) );
--- sub_32.vhd
+++ sub_32.vhd
@@ -18,18 +18,16 @@ architecture structural of sub_32 is
 component add_32 is
   port (
     x, y   : in  std_logic_vector(31 downto 0);
+    carry_in : in std_logic;
     z   : out std_logic_vector(31 downto 0);
     overflow : out std_logic
   );
 end component add_32;
 
-signal not_y, negative_y : std_logic_vector(31 downto 0);
-signal add_overflow : std_logic;
-
+signal not_y: std_logic_vector(31 downto 0);
 
 begin
     not_map : not_gate_32 port map (x, not_y);
-    add_one_map : add_32 port map(not_y, x"00000001", negative_y, add_overflow);
-    add_32_map : add_32 port map (x, negative_y, z, overflow);
+    add_32_map : add_32 port map (x, not_y, '1', z, overflow);
     
 end structural;
